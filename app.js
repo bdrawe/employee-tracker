75b1d38eace0ba4c91572f41854dd3adc6a57cc5
B. Drawe <brycerdrawe.b@gmail.com>
2020-10-17 00:54:43 -0600

    initial prompt

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const inquirer = require('inquirer');
 
+
 inquirer
     .prompt({
-        
+        type: "list",
+        name: "choose",
+        message: "What would you like to do?",
+        choices: ["View all Departments", "View all Roles","View all Employees", "Add a department", "Add a Role", "Add an Employee", "Update an Employee Role"]
     })
+    .then(({choose}) => {
+        if(choose === "View all Departments"){
+            console.log("Display Departments");
+        } else if (choose === "View all Roles"){
+            console.log("Display All Roles");
+        } else if (choose === "View All Employees"){
+            console.log("View Employees");
+        } else if (choose === "Add Department"){
+            console.log("Display Department");
+        } else if(choose === "Add a Role"){
+            console.log("All Roles");
+        } else if(choose === "Add an Employee"){
+            console.log("Adding an Employee");
+        } else {
+            console.log("Update an Employee Role")
+        }
+    })
+
